3c1a72a142ca5bde5cbb1aa80c4440b481a1453b
rares-p <panainte.rares02@gmail.com>
2024-05-07 16:17:39 +0300

    Added entities

---
+++ Domain/Entities/Submission.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities;
+
+public class Submission(Guid userId, Guid problemId, uint score, DateTime date)
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public Guid UserId { get; private set; } = userId;
+    public Guid ProblemId { get; private set; } = problemId;
+    public uint Score { get; private set; } = score;
+    public DateTime Date { get; private set; } = date;
+}
---
+++ Domain/Entities/Test.cs
@@ -0,0 +1,37 @@
+﻿using HealthcareManagementSystem.Domain.Common;
+
+namespace Domain.Entities;
+
+public class Test
+{
+    public Guid Id { get; private set; }
+    public Guid ProblemId { get; private set; }
+    public uint Index { get; private set; }
+    public string Input { get; private set; }
+    public string Output { get; private set; }
+    public uint Score { get; private set; }
+
+    private Test(Guid problemId, uint index, string input, string output, uint score)
+    {
+        Id = Guid.NewGuid();
+        ProblemId = problemId;
+        Index = index;
+        Input = input;
+        Output = output;
+        Score = score;
+    }
+
+    public static Result<Test> Create(Guid problemId, uint index, string input, string output, uint score)
+    {
+        if(string.IsNullOrWhiteSpace(input))
+            return Result<Test>.Failure("Test Input cannot be empty!");
+
+        if(string.IsNullOrWhiteSpace(output))
+            return Result<Test>.Failure("Test Input cannot be empty!");
+
+        if(score == 0)
+            return Result<Test>.Failure("Test score must pe a positive number!");
+
+        return Result<Test>.Success(new Test(problemId, index, input, output, score));
+    }
+}
