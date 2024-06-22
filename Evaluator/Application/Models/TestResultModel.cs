79a22a7c9a91b74f8f4e7a8c07a599c1d58e76db
rares-p <panainte.rares02@gmail.com>
2024-06-22 13:54:20 +0300

    why was it ignored

---
+++ Evaluator/Application/Models/TestResultModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Models;
+
+public class TestResultModel
+{
+    public string Message { get; set; } = string.Empty;
+    public uint Score { get; set; }
+    public uint Runtime { get; set; }
+    public uint TestIndex { get; set; }
+}
