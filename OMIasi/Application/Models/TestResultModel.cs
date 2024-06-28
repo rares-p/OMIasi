c74927d98b5d15bfe36f84138be122ccfe2bf2fd
rares-p <panainte.rares02@gmail.com>
2024-06-28 21:33:14 +0300

    Create TestResultModel.cs

---
+++ OMIasi/Application/Models/TestResultModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Application.Models;
+
+public class TestResultModel
+{
+    [JsonPropertyName("message")]
+    public string Message { get; set; } = string.Empty;
+
+    [JsonPropertyName("runtime")]
+    public uint Runtime { get; set; }
+
+    [JsonPropertyName("score")]
+    public uint Score { get; set; }
+
+    [JsonPropertyName("testIndex")]
+    public uint TestIndex { get; set; }
+}
